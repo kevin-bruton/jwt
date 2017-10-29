ba735542c1368d28ac270b2ccfe5c64d4472d2f6
Kevin Bruton <kevin.bruton.contractor@bbva.com>
2017-10-29 21:36:47 +0100

    Update db config info

--- back/config.js
+++ back/config.js
@@ -1,4 +1,4 @@
 module.exports = {
   'secret': 'beryllium',
-  'database': 'mongodb://heroku_9p4s9d5s:wuphQB8smIsUswUlOZ6UTrbPVH-_sTYO@ds137435.mlab.com:37435/heroku_9p4s9d5s'
+  'database': 'mongodb://heroku_tester:sanjuan91@ds137435.mlab.com:37435/heroku_9p4s9d5s'
 };
