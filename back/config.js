73853bdd9bbdde7e108004c920730f50f8ca3fee
Kevin Bruton <kevin.bruton.contractor@bbva.com>
2017-10-29 20:40:00 +0100

    Add heroku db config

--- back/config.js
+++ back/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  'secret': 'secret',
-  'database': 'mongodb://localhost:27017/testdb'
+  'secret': 'beryllium',
+  'database': 'mongodb://heroku_9p4s9d5s:wuphQB8smIsUswUlOZ6UTrbPVH-_sTYO@ds137435.mlab.com:37435/heroku_9p4s9d5s'
 };
